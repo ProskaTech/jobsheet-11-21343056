d90aed38b74b7b87f8eac00ebb7a07f14d456966
Muhammad Asyrof <112678905+21343056@users.noreply.github.com>
2022-11-09 17:45:50 +0800

    Delete src directory

--- src/latihan/latihan2/KonstruktorSuperKelas.java
+++
@@ -1,9 +0,0 @@
-package latihan.latihan2;
-
-    public class KonstruktorSuperKelas {
-        public static void main(String[] args) {
-            Employ programer1 = new Employ ("12345678", "Budi", 33);
-            programer1.info();
-        }
-
-    }
